fc5d89c3697507cdc0cef261833cd589bbe40c44
Sven Lindauer <sven.lindauer@inovex.de>
2020-07-09 11:45:56 +0200

    Initialize project

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	let name = 'world';
+	let characters =  [];
+
+	let test = () => fetch('https://rickandmortyapi.com/api/character/').then(response => response.json())
+  			.then(data => {
+			characters = data.results
+			});
+</script>
+
+<h1>Hello {name}!</h1>
+<button on:click={test}>click me</button>
+<ul>
+	{#each characters as character}
+		<li>{character.name}</li>
+	{/each}
+</ul>
